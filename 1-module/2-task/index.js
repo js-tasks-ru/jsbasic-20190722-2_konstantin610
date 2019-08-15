22e360d1cdae957bbb771932e5d68ec70394204a
Konstantin.Kirillov <kirilloffkostya@gmail.com>
2019-08-15 23:24:49 +0300

    Решение задачи №2 модель 1 и задачи №1 модуль 2

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,6 +10,7 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
+  return (name !== undefined) && (name !== '') && (name !== null) && (name.indexOf(' ') < 0) && (name.length >= 4);
 }
 
 function sayHello() {
--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,4 +4,17 @@
  * @returns {Object}
  */
 function clone(obj) {
+  const cloneObj = {};
+
+  for (const key in obj) {
+    const prop = obj[key];
+
+    if (typeof (prop) === 'object' && (prop !== null)) {
+      cloneObj[key] = clone(prop);
+    } else {
+      cloneObj[key] = prop;
+    }
+  }
+
+  return cloneObj;
 }
