9d4a81b57f6b9cba80f1ab73690c04302e27e215
Konstantin.Kirillov <kirilloffkostya@gmail.com>
2019-08-28 18:53:25 +0300

    Решение задач модуля №3

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,4 +4,11 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
+  const arrayTotal = str.split(' ').join(',').split(',').filter(item => !(item === '')
+    && Number.isFinite(Number(item)));
+  const result = {};
+  result.max = Math.max(...arrayTotal);
+  result.min = Math.min(...arrayTotal);
+
+  return result;
 }
