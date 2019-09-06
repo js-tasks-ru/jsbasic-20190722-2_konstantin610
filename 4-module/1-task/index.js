e918495e00c8e45e58b5b44e518795e6235cdf0e
Konstantin Kirillov <Konstantin.Kirillov@miningelement.com>
2019-09-06 15:30:59 +0300

    Решение задачи для занятия №4

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,4 +4,12 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
+  const ulElement = document.createElement('ul');
+
+  friends.forEach((item) => {
+    const liElement = document.createElement('li');
+    liElement.append(`${item.firstName} ${item.lastName}`);
+    ulElement.append(liElement);
+  });
+  return ulElement;
 }
